503a6ac35901fb182de637faef18904ca35668e4
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-05-18 22:50:04 +0530

    Added: pop first node

--- src/main/java/com/datastructure/MyLinkedList.java
+++ src/main/java/com/datastructure/MyLinkedList.java
@@ -33,9 +33,15 @@ public class MyLinkedList<T> {
             tail = newNode;
         }
     }
+
     public void insertNode(INode<Integer> myFirstNode, INode<Integer> mySecondNode) {
         mySecondNode.setNext(myFirstNode.getNext());
         myFirstNode.setNext(mySecondNode);
+    }
+
+    /* Delete/Pop First Node */
+    public void pop() {
+        this.head = this.head.getNext();
     }
 
 
