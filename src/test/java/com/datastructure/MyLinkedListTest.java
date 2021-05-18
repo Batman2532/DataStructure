3337153e0559590c23728c403a0bd820cacec874
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-05-19 00:44:04 +0530

    Added: create stack

--- src/test/java/com/datastructure/MyLinkedListTest.java
+++ src/test/java/com/datastructure/MyLinkedListTest.java
@@ -147,4 +147,17 @@ public class MyLinkedListTest {
                 && myLinkedList.head.getNext().getNext().equals(myFirstNode) && myLinkedList.tail.equals(myThirdNode);
         Assertions.assertEquals(true, result);
     }
+
+    @Test
+    public void IfStackIsCreatedShouldReturnTrue() {
+        Stack<Integer> stack = new Stack<>();
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        stack.push(myThirdNode);
+        stack.push(mySecondNode);
+        stack.push(myFirstNode);
+        INode peek = stack.peek();
+        Assertions.assertEquals(peek, myFirstNode);
+    }
 }
